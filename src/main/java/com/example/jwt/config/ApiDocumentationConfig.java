f4dbdba5139bfeb47af6e49a5f57259ed4289231
NEWFOUND SYSTEMS <newfound-systems@users.noreply.github.com>
2020-01-17 15:46:15 +0530

    Update ApiDocumentationConfig.java
    Swagger Definition Changes

--- src/main/java/com/example/jwt/config/ApiDocumentationConfig.java
+++ src/main/java/com/example/jwt/config/ApiDocumentationConfig.java
@@ -2,9 +2,6 @@
  * Copyright NEWFOUND SYSTEMS to Present
  * All Rights Reserved
  */
-/**
- * 
- */
 package com.example.jwt.config;
 
 import io.swagger.annotations.Contact;
@@ -15,23 +12,23 @@ import io.swagger.annotations.SwaggerDefinition;
 
 @SwaggerDefinition(
         info = @Info(
-                description = "DHL Api Services",
+                description = "Api Services",
                 version = "v1.0.0",
-                title = "DHL Api",
+                title = "Api",
                 contact = @Contact(
                    name = "NEWFOUND SYSTEMS", 
                    email = "support@newfound-systems.com", 
                    url = "http://www.newfound-systems.com"
                 ),
                 license = @License(
-                   name = "DHL Express", 
-                   url = "http://www.dhl.com"
+                   name = "NEWFOUND SYSTEMS", 
+                   url = "http://www.newfound-systems.com"
                 )
         ),
         consumes = {"application/json"},
         produces = {"application/json"},
         schemes = {SwaggerDefinition.Scheme.HTTPS},
-        externalDocs = @ExternalDocs(value = "Read this", url = "http://www.dhl.com")
+        externalDocs = @ExternalDocs(value = "Read this", url = "http://www.newfound-systems.com")
 )
 public interface ApiDocumentationConfig {
 }
